bc636f6b5c4b04c8f3d335840c0ce844c12e83e6
Thomas Rosendal <thomas.rosendal@sva.se>
2018-10-26 09:05:21 +0200

    As.data.frame arguments
    Arguments need to be consistentr with the as.data.frame methods
    defined elsewhere

--- R/points.R
+++ R/points.R
@@ -84,9 +84,12 @@ svis_points.data.frame <- function(data, crs = NULL, ...) {
 ##' as.data.frame.svis_points
 ##'
 ##' @param x A svis_points_object
+##' @param row.names the row names
+##' @param optional Optional arguements to the data.frame method
+##' @param ... Other arguments
 ##' @export
 ##' @return A data.frame
-as.data.frame.svis_points <- function(x) {
+as.data.frame.svis_points <- function(x, row.names, optional, ...) {
     do.call("rbind", lapply(x, function(y){
         class(y) <- "list"
         as.data.frame(y)
